0386466b2c47e375cc2df896e48e92ef534dc0c3
mskwarek <m.skwarek@stud.elka.pw.edu.pl>
2016-08-14 07:50:55 -0400

    multiprocessing

--- python_module/axfrWrapper.py
+++ python_module/axfrWrapper.py
@@ -1,4 +1,5 @@
 from ctypes import cdll
+import time 
 lib = cdll.LoadLibrary('../build/lib/libAxfrLib.so')
 
 class axfrLookup(object):
@@ -15,18 +16,33 @@ class axfrLookup(object):
     def destroy(self):
         lib.axfrLookup_destroy(self.obj)
 
-
-        
-
 def perform_lookup(domain, ns):
     x = axfrLookup()
     x.getResult(domain, ns)
     x.destroy()
 
-from multiprocessing import Process, Queue
+def start_new_process(domain, ns):
+    p = Process(target=perform_lookup, args=(domain, ns))    
+    p.start()
+
+def try_to_start_process(domain, ns):
+    yield "dupa"
+    
+from multiprocessing import Process, Queue, Lock
 
 if __name__ == '__main__':
-    for i in range(0,3):
-        p = Process(target=perform_lookup, args=("infusionsoft.com", "ns3.infusionsoft.com"))
-        p.start()
-        p.join() # this blocks until the process terminates
+    input_list = [line.rstrip('\n').split(' ') for line in open('inputData')]
+    j = 0;
+    for i in input_list:
+        print j, " of ", len(input_list)
+        j+=1
+        while True:
+            try:
+                start_new_process(i[0], i[1])
+                break
+            except:
+                print "wait"
+                time.sleep(1)
+        
+            #p.join()
+        #p.join() # this blocks until the process terminates
