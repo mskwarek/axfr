97fa9bab8c9c3042536b690209fdd108f429d157
Marcin Skwarek <marcin@biedronka>
2017-03-29 23:28:19 +0200

    x* output from scanner to sorted folders

--- mydns/run_all.sh
+++ mydns/run_all.sh
@@ -1,4 +1,8 @@
+j=0
+
 for i in /home/marcin/myDig/mydns/splitted_data2/*.*
 do
-    nohup ./a.out $i 1 &
+    j=$[$j+1]
+    mkdir -p test/iter_$j
+    nohup ./a.out $i 3 /home/marcin/myDig/mydns/test/iter_$j &
 done
