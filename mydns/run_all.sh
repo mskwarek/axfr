509f89b9944475a78d7ac811cab97434241e51c2
Marcin <m.skwarek@stud.elka.pw.edu.pl>
2017-04-02 00:50:00 +0200

    * madrzejsze zrzucanie wynikow

--- mydns/run_all.sh
+++ mydns/run_all.sh
@@ -1,8 +1,13 @@
 j=0
 
-for i in /home/marcin/myDig/mydns/splitted_data2/*.*
+mkdir /home/marcin/myDig/splitted
+split -l 100 /home/marcin/myDig/mydns/inputData2_ip /home/marcin/myDig/splitted/inp.
+datetime=`date "+%Y_%m_%d_%H_%M"`
+
+
+for i in /home/marcin/myDig/splitted/*.*
 do
     j=$[$j+1]
-    mkdir -p test/iter_$j
-    nohup ./a.out $i 3 /home/marcin/myDig/mydns/test/iter_$j &
+    mkdir -p /home/marcin/myDig/mydns/test_$datetime/iter_$j
+    nohup ./a.out $i 3 /home/marcin/myDig/mydns/test_$datetime/iter_$j &
 done
