61347c2b6c8c90d2d0b83cbdf0b47c8d836e74c0
marcin <m.skwarek@stud.elka.pw.edu.pl>
2017-04-11 23:41:05 +0200

    * run_all with proces count

--- mydns/run_all.sh
+++ mydns/run_all.sh
@@ -1,13 +1,22 @@
+#!/bin/bash
+
 j=0
 
-mkdir /home/marcin/myDig/splitted
-split -l 100 /home/marcin/myDig/mydns/inputData2_ip /home/marcin/myDig/splitted/inp.
+#mkdir /home/marcin/myDig/splitted
+#split -l 100 /home/marcin/myDig/mydns/inputData2_ip /home/marcin/myDig/splitted/inp.
 datetime=`date "+%Y_%m_%d_%H_%M"`
 
 
-for i in /home/marcin/myDig/splitted/*.*
+for i in {1..10}
 do
-    j=$[$j+1]
-    mkdir -p /home/marcin/myDig/mydns/test_$datetime/iter_$j
-    nohup ./a.out $i 3 /home/marcin/myDig/mydns/test_$datetime/iter_$j &
+    x=`ps -aux | grep a.out | grep -v grep | wc -l`
+    
+    if [ "1000" -gt "$x" ]; then
+        echo $x
+    
+        j=$[$j+1]
+        #mkdir -p /home/marcin/myDig/mydns/test_$datetime/iter_$j
+        #nohup ./a.out $i 3 /home/marcin/myDig/mydns/test_$datetime/iter_$j &
+        sleep 10
+    fi
 done
