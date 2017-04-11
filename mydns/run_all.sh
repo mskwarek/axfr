029547b5af164e41b8df92fdaf240d07b10627aa
marcin <m.skwarek@stud.elka.pw.edu.pl>
2017-04-12 00:37:31 +0200

    * profi run_all

--- mydns/run_all.sh
+++ mydns/run_all.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 j=0
+PROCMAX=100
+OUTPUTDIR=/home/marcin/ClionProjects/myDig/mydns
+INPUTDIR=/home/marcin/ClionProjects/myDig/splitted
 
 #mkdir /home/marcin/myDig/splitted
 #split -l 100 /home/marcin/myDig/mydns/inputData2_ip /home/marcin/myDig/splitted/inp.
-datetime=`date "+%Y_%m_%d_%H_%M"`
 
+shopt -s nullglob
+array=($INPUTDIR/*.*)
+shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
+#echo "${array[@]}"
+tlen=${#array[@]}
 
-for i in {1..10}
+for (( i=0; i<${tlen}; ));
 do
     x=`ps -aux | grep a.out | grep -v grep | wc -l`
     
-    if [ "1000" -gt "$x" ]; then
-        echo $x
-    
+    if [ "$PROCMAX" -gt "$x" ]; then
+        echo "start couse x= $x"
         j=$[$j+1]
-        #mkdir -p /home/marcin/myDig/mydns/test_$datetime/iter_$j
-        #nohup ./a.out $i 3 /home/marcin/myDig/mydns/test_$datetime/iter_$j &
-        sleep 10
+        datetime=`date "+%Y_%m_%d_%H_%M"`
+        mkdir -p $OUTPUTDIR/test_$datetime/iter_$j
+        nohup ./a.out ${array[$i]} 3 $OUTPUTDIR/test_$datetime/iter_$j &
+        sleep 1
+        i=$[$i+1]
     fi
 done
