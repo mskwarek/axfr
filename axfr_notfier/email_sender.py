ea6ccaec2aa5608c6cbbabc0de606a01716acda2
Marcin <m.skwarek@stud.elka.pw.edu.pl>
2017-04-28 21:59:59 +0200

    * added info about vulnerable domain

--- axfr_notfier/email_sender.py
+++ axfr_notfier/email_sender.py
@@ -15,13 +15,17 @@ class EmailSender(object):
         self.server.starttls()
         self.server.login(self.data['from'], self.data['password'])
 
-    def send_msg(self, TO):
+    def send_msg(self, TO, dom, IP):
         SUBJECT = "DNS server issue"
         TEXT = """Hi,\n\nMy name is {myName} and I am a Telecommunications student at Warsaw Uniwersity of Technology. \
 I am doing a research on DNS servers and I noticed that your site is vulnerable to unauthorized AXFR zone transfer. \
-It means that anyone at the Internet can see your DNS server's records. You can google what that could cause.\n\n\
+It means that anyone at the Internet can see your DNS server's records. You can google what that could cause. Below I\
+send you the data that could identify your vulnerable server:\n
+domain name: {domain}\n
+nameserver IP: {nsIp}\n
+I would be grateful if you for any feedback from you.\n\n\
 ---\n{myName}\n{organization}\n{university}""".format(myName=self.data['myName'], organization=self.data['organization'],
-                                                      university=self.data['uni'])
+                                                      university=self.data['uni'], nsIp=IP, domain=dom)
 
         message = """From: %s\r\nTo: %s\r\nSubject: %s\r\n\
 
