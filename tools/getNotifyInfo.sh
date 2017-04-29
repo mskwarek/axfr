8150669ef6cc1cf3820ab3c37c58a3c855bc2ae4
Marcin <m.skwarek@stud.elka.pw.edu.pl>
2017-04-29 14:37:15 +0200

    * added file generation with nofity info

---
+++ tools/getNotifyInfo.sh
@@ -0,0 +1,11 @@
+for file in /media/marcin/may/test_2017_04_24/iter_005*/*_*.axfr;do
+    soa=`cat $file | grep -v 'pasuje do wzorca' | grep -P '\t6\t'`
+    if [[ $soa = *[!\ ]* ]]; then
+	person=`echo $soa | cut -d ' ' -f 5`
+	fileName=`echo $file | rev | cut -d '/' -f 1 | rev | sed 's/.axfr//g'`
+	domain=`echo $fileName | cut -d _ -f 1`
+	nameserver=`echo $fileName | cut -d _ -f 2`
+	echo $domain $nameserver $person
+    fi
+
+done
