2b7a35a66db6fc816b3081923cf88f9a54941933
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2017-06-03 14:31:51 +0200

    * improvements in moving script

--- tools/mvUnused.sh
+++ tools/mvUnused.sh
@@ -1,7 +1,7 @@
 input=$1
 output=$2
 
-array=`find $input -type f -size 25123c -exec ls {} \;`
+array=`find $input -type f -size 77824c -exec ls {} \;`
 for item in ${array[@]}; do
     mv $item $output/
 done
