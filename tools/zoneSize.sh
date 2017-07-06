f89727e4b2231a662edbd816a97b96a2a5c70e8c
marcin <m.skwarek@stud.elka.pw.edu.pl>
2017-07-06 20:43:13 +0200

    * zoneSize improvement

--- tools/zoneSize.sh
+++ tools/zoneSize.sh
@@ -2,21 +2,24 @@
 processDir(){
     array=`find $3/$1 -type f`
     sufix=`echo $1 | sed 's/test_//g'`
+    baseResultDir=$2
+    mkdir ${baseResultDir}/oneLines/${sufix}
+    mkdir ${baseResultDir}/manyLines/${sufix}
     for file in ${array[@]}; do
 	fileLines=`cat $file | wc -l`
         baseResultDir=$2
 	if [ "$fileLines" -eq "1" ]
 	then
-	    dir=${baseResultDir}/oneLines
+	    dir=${baseResultDir}/oneLines/${sufix}
 	    tld=${baseResultDir}/oneTlds_${sufix}.result
 	    nsIp=${baseResultDir}/oneNsIp_${sufix}.result
 	else
-	    dir=${baseResultDir}/manyLines
+	    dir=${baseResultDir}/manyLines/${sufix}
 	    tld=${baseResultDir}/manyTlds_${sufix}.result
 	    nsIp=${baseResultDir}/manyNsIp_${sufix}.result
 	fi
 	filename=`basename $file`
-	cp $file $dir/$filename
+	mv $file $dir/$filename
 	echo $file | rev | cut -d '/' -f 1 | rev | cut -d _ -f 1 | rev | cut -d . -f 1 | rev >> $tld 
 	echo $file | rev | cut -d'_' -f1 | rev | sed 's/.axfr//g' >> $nsIp
     done
