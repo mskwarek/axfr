d76860a64a356853e12052231b69ecd5f0e24c05
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2017-07-28 22:06:30 +0200

    * more more more

--- tools/world_map.py
+++ tools/world_map.py
@@ -5,17 +5,21 @@ py.sign_in('mskwarek', 'gw9wh5r9h2')
 #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
 
 
-df = pd.read_csv('/Users/mskwarek/Documents/myDig/results/countriesCnt_iso.result')
+#df = pd.read_csv('/Users/mskwarek/Documents/myDig/results/countriesCnt_iso.result')
+df = pd.read_csv('/Users/mskwarek/Documents/myDig/results/countriesNsManyLinesIp.result')
+#df = pd.read_csv('/Users/mskwarek/Documents/myDig/results/countriesCnt_used.result')
+
 
 data = [ dict(
         type = 'choropleth',
         locations = df['CODE'],
         z = df['CNT'],
         text = df['COUNTRY'],
-        colorscale = [[0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
-            [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"]],
+        colorscale=[[0,"rgb(142,174,96)"],[0.05,"rgb(215,234,84)"],
+                    [0.15,"rgb(222,228,63)"],[0.3,"rgb(234,179,72)"],
+                    [0.6,"rgb(234,102,72)"],[1,"rgb(167,57,31)"]],
         autocolorscale = False,
-        reversescale = True,
+        reversescale = False,
         marker = dict(
             line = dict (
                 color = 'rgb(180,180,180)',
@@ -28,7 +32,7 @@ data = [ dict(
       ) ]
 
 layout = dict(
-    title = '2017',
+    title = 'Lokalizacja serwerow DNS odpowiadajacych wiecej niz jednym rekordem',
     geo = dict(
         showframe = False,
         showcoastlines = False,
@@ -39,4 +43,4 @@ layout = dict(
 )
 
 fig = dict( data=data, layout=layout )
-py.iplot( fig, validate=False, filename='d3-world-map' )
+py.iplot( fig, validate=False, filename='d3-world-map-na-many-lines' )
