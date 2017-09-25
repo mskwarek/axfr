c848e0873a21c03d19282c188e868e01469903ad
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2017-09-25 23:29:11 +0200

    * wip

--- mydns/dns.c
+++ mydns/dns.c
@@ -72,6 +72,28 @@ struct DNS_HEADER
 };
 typedef struct DNS_HEADER DNS_H;
 
+struct DNS_HEADER_UDP
+{
+    unsigned short id; // identification number
+
+    unsigned char rd :1; // recursion desired
+    unsigned char tc :1; // truncated message
+    unsigned char aa :1; // authoritive answer
+    unsigned char opcode :4; // purpose of message
+    unsigned char qr :1; // query/response flag
+
+    unsigned char rcode :4; // response code
+    unsigned char cd :1; // checking disabled
+    unsigned char ad :1; // authenticated data
+    unsigned char z :1; // its z! reserved
+    unsigned char ra :1; // recursion available
+
+    unsigned short q_count; // number of question entries
+    unsigned short ans_count; // number of answer entries
+    unsigned short auth_count; // number of authority entries
+    unsigned short add_count; // number of resource entries
+};
+
 struct QUESTION
 {
     unsigned short qtype;
@@ -162,36 +184,34 @@ dns_result ngethostbyname(const char *que , const char *server, const char *dst_
     snprintf(host, 128, "%s", que);
     // printf("\nResolving %s\n" , host);
 
-    dns = (struct DNS_HEADER *)&buf;
-
-    dns_id = getpid();
-    dns->id = (unsigned short) htons(dns_id);
-    dns->qr = 0; //This is a query
-    dns->opcode = 0; //This is a standard query
-    dns->aa = 0; //Not Authoritative
-    dns->tc = 0; //This message is not truncated
-    dns->rd = 1; //Recursion Desired
-    dns->ra = 0; //Recursion not available! hey we dont have it (lol)
-    dns->z = 0;
-    dns->ad = 0;
-    dns->cd = 0;
-    dns->rcode = 0;
-    dns->q_count = htons(1); //we have only 1 question
-    dns->ans_count = 0;
-    dns->auth_count = 0;
-    dns->add_count = 0;// htons(1);
-
-    //point to the query portion
-    qname =(unsigned char*)&buf[sizeof(struct DNS_HEADER)];
-    ChangetoDnsNameFormat(qname , host);
-    qinfo =(struct QUESTION*)&buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname) + 1)]; //fill it
-    qinfo->qtype = htons( query_type ); //type of the query , A , MX , CNAME , NS etc
-    qinfo->qclass = htons(1);
-    dns->len = htons((unsigned int)sizeof(struct DNS_HEADER) + (strlen((const char*)qname)+1) + sizeof(struct QUESTION)-2);
-
-
     if(TRANSPORT_TYPE_UDP == transport_type)
     {
+        dns = (struct DNS_HEADER_UDP *)&buf;
+
+        dns_id = getpid();
+        dns->id = (unsigned short) htons(dns_id);
+        dns->qr = 0; //This is a query
+        dns->opcode = 0; //This is a standard query
+        dns->aa = 0; //Not Authoritative
+        dns->tc = 0; //This message is not truncated
+        dns->rd = 1; //Recursion Desired
+        dns->ra = 0; //Recursion not available! hey we dont have it (lol)
+        dns->z = 0;
+        dns->ad = 0;
+        dns->cd = 0;
+        dns->rcode = 0;
+        dns->q_count = htons(1); //we have only 1 question
+        dns->ans_count = 0;
+        dns->auth_count = 0;
+        dns->add_count = 0;// htons(1);
+
+        //point to the query portion
+        qname =(unsigned char*)&buf[sizeof(struct DNS_HEADER)];
+        ChangetoDnsNameFormat(qname , host);
+        qinfo =(struct QUESTION*)&buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname) + 1)]; //fill it
+        qinfo->qtype = htons( query_type ); //type of the query , A , MX , CNAME , NS etc
+        qinfo->qclass = htons(1);
+
         int i = 0;
         s = socket(AF_INET , SOCK_DGRAM , IPPROTO_UDP); //UDP packet for DNS queries
         dest.sin_family = AF_INET;
@@ -217,6 +237,35 @@ dns_result ngethostbyname(const char *que , const char *server, const char *dst_
     }
     else
     {
+
+        dns = (struct DNS_HEADER *)&buf;
+
+        dns_id = getpid();
+        dns->id = (unsigned short) htons(dns_id);
+        dns->qr = 0; //This is a query
+        dns->opcode = 0; //This is a standard query
+        dns->aa = 0; //Not Authoritative
+        dns->tc = 0; //This message is not truncated
+        dns->rd = 1; //Recursion Desired
+        dns->ra = 0; //Recursion not available! hey we dont have it (lol)
+        dns->z = 0;
+        dns->ad = 0;
+        dns->cd = 0;
+        dns->rcode = 0;
+        dns->q_count = htons(1); //we have only 1 question
+        dns->ans_count = 0;
+        dns->auth_count = 0;
+        dns->add_count = 0;// htons(1);
+
+        //point to the query portion
+        qname =(unsigned char*)&buf[sizeof(struct DNS_HEADER)];
+        ChangetoDnsNameFormat(qname , host);
+        qinfo =(struct QUESTION*)&buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname) + 1)]; //fill it
+        qinfo->qtype = htons( query_type ); //type of the query , A , MX , CNAME , NS etc
+        qinfo->qclass = htons(1);
+        dns->len = htons((unsigned int)sizeof(struct DNS_HEADER) + (strlen((const char*)qname)+1) + sizeof(struct QUESTION)-2);
+
+
         s = socket(AF_INET , SOCK_STREAM , IPPROTO_TCP);
         if(s<0)
         {
