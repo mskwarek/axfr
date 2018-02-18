b7b91e5099dae670ab8402fb1654ce89ba02527d
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-02-19 00:15:14 +0100

    * mocks in tests uncommented

--- mydns/test/cmock-spec-builders_test.cc
+++ mydns/test/cmock-spec-builders_test.cc
@@ -2,12 +2,14 @@
 
 #include "foo_mock.h"
 
-TEST(SpecBuildersTest, ExpectFunctionCallCompiles) {
+TEST(SpecBuildersTest, ExpectFunctionCallCompiles)
+{
     FooFunctionMock mock;
     EXPECT_FUNCTION_CALL(mock, (1, 2)).Times(0);
 }
 
-TEST(SpecBuildersTest, OnFunctionCallCompiles) {
-//  FooFunctionMock mock;
-//  ON_FUNCTION_CALL(mock, (1, 2));
+TEST(SpecBuildersTest, OnFunctionCallCompiles)
+{
+    FooFunctionMock mock;
+    ON_FUNCTION_CALL(mock, (1, 2));
 }
