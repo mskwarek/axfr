b8cac4d0109fc1eecd4151333da63cf356840e0f
Marcin Skwarek <m.skwarek@stud.elka.pw.edu.pl>
2018-06-21 18:22:42 +0200

    * refactor

--- mydns/DnsQuery/test/DnsTcpRequestSenderTest.cpp
+++ mydns/DnsQuery/test/DnsTcpRequestSenderTest.cpp
@@ -14,25 +14,28 @@ using namespace ::testing;
 
 class DnsTcpRequestSenderTest : public ::testing::Test
 {
+public:
+    unsigned char buf[65535] = {0};
+    unsigned char* qname = NULL;
+    struct QUESTION *qinfo = NULL;
+    DNS_H_TCP* dns = (struct DNS_TCP_HEADER *)&buf;
+    char *query = "www.google.com";
+    char host[128] = {0};
+
+
+    virtual void setUp()
+    {
+        snprintf(host, 128, "%s", query);
+
+        feel_dns_header_req(&(dns->header));
+        qname = &buf[sizeof(struct DNS_TCP_HEADER)];
+    }
 
 };
 
 TEST_F(DnsTcpRequestSenderTest, testSendingTcpRequest)
 {
-
-    unsigned char buf[65535] = {0};
-    DNS_H_TCP* dns = (struct DNS_TCP_HEADER *)&buf;
-    struct QUESTION *qinfo = NULL;
-
-    char *query = "www.google.com";
-
-    char host[128] = {0};
-
-    snprintf(host, 128, "%s", query);
-
-    feel_dns_header_req(&(dns->header));
-    unsigned char* qname = &buf[sizeof(struct DNS_TCP_HEADER)];
-
+    setUp();
     dns_tcp_req(dns, qname, qinfo, 30,  host,
                            buf, QTYPE_AXFR, "192.168.0.1");
 }
@@ -41,19 +44,7 @@ TEST_F(DnsTcpRequestSenderTest, testReturningErrorWhenNoSocketIsCreated)
 {
     SocketCreateMock socketMock;
     EXPECT_FUNCTION_CALL(socketMock, (_, _, _)).WillOnce(Return(-1));
-
-    unsigned char buf[65535] = {0};
-    DNS_H_TCP* dns = (struct DNS_TCP_HEADER *)&buf;
-    struct QUESTION *qinfo = NULL;
-
-    char *query = "www.google.com";
-
-    char host[128] = {0};
-
-    snprintf(host, 128, "%s", query);
-
-    feel_dns_header_req(&(dns->header));
-    unsigned char* qname = &buf[sizeof(struct DNS_TCP_HEADER)];
+    setUp();
 
     EXPECT_EQ(DNS_RESULT_ERR, dns_tcp_req(dns, qname, qinfo, 30,  host,
                                           buf, QTYPE_AXFR, "192.168.0.1"));
